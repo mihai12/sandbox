ffaaef8036bdd666594c5d03b50883e6c8cd1f28
mihai12 <mihai.popa@windowslive.com>
2015-02-06 21:14:47 +0200

    Loadmore plugin

--- jquery-loadmore/articles.php
+++ jquery-loadmore/articles.php
@@ -1,14 +1,10 @@
 <?php
-ini_set('display_startup_errors',1); //show all errors
-ini_set('display_errors',1);
-error_reporting(-1);
-
 //change the content type of the output
 header('Content-Type: application/json');
 
-$db = new PDO('mysql:host=localhost;dbname=mihaipco_sandbox',
-              'mihaipco_sites',
-              'Li1B3c13~vzW');
+$pdo = new PDO('mysql:host=localhost;dbname=sandbox',
+              'root',
+              '');
 
 $articles = [];
 
@@ -19,5 +15,16 @@ $article = $pdo->query("SELECT SQL_CALC_FOUND_ROWS *
                         FROM articles
                         LIMIT {$start}, {$count}");
 
-echo $articlesTotal = $pdo->query("SELECT FOUND_ROWS()
-                                   AS count")->fetch(PDO::FETCH_ASSOC)['count'];  
+$articlesTotal = $pdo->query("SELECT FOUND_ROWS()
+                              AS count")->fetch(PDO::FETCH_ASSOC)['count'];
+
+if($articlesCount = $article->rowCount()) {
+      $articles = $article->fetchAll(PDO::FETCH_OBJ);
+}
+
+echo json_encode(array(
+    'items'=> $articles,
+    'last' => ($start + $count) >= $articlesTotal ? true : false,
+    'start' => $start,
+    'count' => $count
+));
